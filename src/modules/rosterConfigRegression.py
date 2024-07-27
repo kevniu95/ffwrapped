5cb3200a983d1012a17a4d9d03424d213b08f890
kevniu95 <kniu@uchicago.edu>
2024-07-27 15:33:03 -0500

    Add regression by position then evaluate overall

--- src/modules/rosterConfigRegression.py
+++ src/modules/rosterConfigRegression.py
@@ -48,14 +48,16 @@ def createDfRow(name : str,
                  model: str,
                  time_to_finish: float,
                  train_r2: float,
-                 test_r2: float
+                 test_r2: float,
+                 position: str = None
                  ) -> pd.DataFrame:
     return pd.DataFrame({'Name': [name],
                           'Description': [description],
                           'Model': model, 
                           'TimeToFinish': [time_to_finish],
                           'TrainR2': [train_r2],
-                          'TestR2': [test_r2]
+                          'TestR2': [test_r2],
+                          'Position': [position]
                       })
 
 
@@ -81,7 +83,7 @@ def _mapRosterConfigToStarter(df: pd.DataFrame) -> pd.DataFrame:
     df3['final_pred_points'] = df3['A_pred_points'] + df3['B_pred_points']
     return df3[['team', scoringType.points_name(), 'final_pred_points']]
 
-def _prepStarterLinearRegresion(path: str = ROSTER_CONFIG_REGRESSION_DATA) -> pd.DataFrame:
+def _prepStarterLinearRegression(path: str = ROSTER_CONFIG_REGRESSION_DATA) -> pd.DataFrame:
     return _mapRosterConfigToStarter(uploadSource(path))
                         
 def doRosterConfigStarter(save_coefs: bool = True,
@@ -91,7 +93,7 @@ def doRosterConfigStarter(save_coefs: bool = True,
     name = "Starter"
     x_description = '''One variable: total final predicted points of starters'''
     
-    df = _prepStarterLinearRegresion()
+    df = _prepStarterLinearRegression()
     for poly in [1, 2, 3]:
         model_res, train_r2, test_r2, time_to_finish = split_and_try_model(df, scoringType.points_name(), ['final_pred_points'], polys = poly, regressor = model)
         model_name = getModelName(model, poly, shortVersion=False)
@@ -141,7 +143,7 @@ def doRosterConfigOneLine(save_coefs:bool = True,
     x_description = """Each team is represented by one single row of all roster config variables"""
     
     df, col_list = _prepOneLineDataFrame()
-    for poly in [1, 2]:
+    for poly in [1, 2, 3]:
         model_res, train_r2, test_r2, time_to_finish = split_and_try_model(df, scoringType.points_name(), col_list, polys = poly, regressor = model)
         model_name = getModelName(model, poly, shortVersion=False)
         df_row = createDfRow(name, x_description, model_name, time_to_finish, train_r2, test_r2)
@@ -155,15 +157,20 @@ def doRosterConfigOneLine(save_coefs:bool = True,
 # =====================
 # Simple
 # =====================
-def _prepSimple() -> Tuple[pd.DataFrame, List[str]]:
-    og_df = uploadSource()
-    df = og_df[og_df['FantPos'].isin(['QB','RB','WR','TE'])].copy()
+def _prepSimple() -> pd.DataFrame:
+    df = uploadSource()
     df['total_points']  = df['num_others'] * df['avg_pred_points_other'] + df['A_pred_points'] + df['B_pred_points']
     df['total_players'] = df['num_others'] + 1 + df['B_pred_points'].apply(lambda x: 1 if x > 0 else 0)
     df['average_points'] = df['total_points'] / df['total_players']
     df['position_variance'] = (df['num_others'] * (df['avg_std_error_other'] ** 2)) + df['A_std_error'] ** 2 + df['B_std_error'].replace(10000, 0) ** 2
-    df['std_error'] = df['position_variance'] ** 0.5
+    df['std_error'] = df['position_variance'] ** 0.5    
+    return df
+
+def _prepSimplePivot() -> Tuple[pd.DataFrame, List[str]]:
+    og_df = uploadSource()
+    df = _prepSimple()
     
+    df = df[df['FantPos'].isin(['QB','RB','WR','TE'])].copy()
     pivot_df = df.pivot_table(index=['year', 'league', 'team'], 
                           columns='FantPos', 
                           values=['average_points', 'total_players', 'std_error'], 
@@ -187,7 +194,7 @@ def doRosterConfigSimple(save_coefs:bool = True,
     name = "Simple"
     x_description = """Each team is represented average and total variance at each position"""
     
-    df, col_list = _prepSimple()
+    df, col_list = _prepSimplePivot()
     for poly in [1, 2]:
         model_res, train_r2, test_r2, time_to_finish = split_and_try_model(df, scoringType.points_name(), col_list, polys = poly, regressor = model)
         model_name = getModelName(model, poly, shortVersion=False)
@@ -200,107 +207,102 @@ def doRosterConfigSimple(save_coefs:bool = True,
             joblib.dump(model_res, save_coefs_path.format(results_file_name))             
 
 
-
-
-
-
-
-# ==================
+# *******
+# ==========================================
 # By position
-# ==================
+# ==========================================
+# *******
 
-def doRosterConfigLinearRegression(save: bool = True):
-    df = uploadSource()
-    for position in ['QB','RB','WR','TE','FLEX']:
-        print('====================')
-        print(position)
-        print('====================')
-        df_sub = df.loc[df['FantPos'] == position].copy()
-        # best_model = None
-        # best_model_r2 = 0
-        for poly in [1]:
-            print(f'Polys: {poly}')
-            model_results = split_and_try_model(df_sub, scoringType.points_name(), ['A_pred_points'], polys = poly)
-            # print(model_results)
-            # if model_results[2] > best_model_r2:
-            # best_model_r2 = model_results[1]
-            best_model = model_results[0]
-        if save:
-            print('dumping results...')
-            path_name = f'../../data/regression/rosterConfig/results/linear_{position}_rosterconfigreg_params.joblib'
-            joblib.dump(best_model, path_name)
-        
-        df_sub['pred'] = best_model.predict(df_sub[['A_pred_points']])
-        df.loc[df_sub['index'], 'pred'] = df_sub['pred']
-        print(df_sub[['year','league','team',scoringType.points_name(), 'pred']].head(25))
-    # print(df.head())
-    ret_df = df[['year','league', 'FantPos','team',scoringType.points_name(), 'pred']]
-    print(ret_df.head(25))
-    a = ret_df.groupby(['year','league','team']).sum().reset_index()
-    print(r2_score(a[scoringType.points_name()], a['pred']))
-    print(a.head())
-    return a
+# =====================
+# Simple Regression
+# =====================
 
-def doRosterConfigNNRegression():
-    pass
-
-def evaluateResults():
-    pass
-
-def doRegressions(scoringType : ScoringType, points_only : bool = False):
-    pos_list = ['QB','RB','WR','TE','FLEX']
-    model_list = [MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.001, learning_rate_init= 0.005),
-                  MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.001, learning_rate_init= 0.001),
-                  MLPRegressor(max_iter=400, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.001, learning_rate_init= 0.001),
-                  MLPRegressor(max_iter=400, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.001, learning_rate_init= 0.001),
-                  MLPRegressor(max_iter=400, hidden_layer_sizes=(150, 150), activation='tanh', solver='adam', alpha=5e-6, learning_rate_init= 0.001)
-                  ]
-    model_dict = dict(zip(pos_list, model_list))
-
-    base_df = pd.read_csv('../../data/regression/rosterConfig/rosterConfigData.csv')
-    base_df.reset_index(inplace = True)
-    for position in pos_list:
-        print(position)
-        reg = model_dict[position]
-        base_df = doRegressionOnPosition(position, base_df, scoringType, True, points_only, polys = 3, regressor = reg)
-    base_df['team_id'] = base_df['index'] // 5 + 1
-    team_res = base_df[['team_id', scoringType.points_name(), 'pred']].groupby(['team_id']).sum().reset_index(drop = True)
-    team_res['var'] = (team_res[scoringType.points_name()] - team_res['pred']) **2
-    print(team_res)
-    print(r2_score(team_res[scoringType.points_name()], team_res['pred']))
-
-def doRegressionOnPosition(position : str, 
-                           base_df : pd.DataFrame, 
-                           scoringType : ScoringType, 
-                           save : bool = True, 
-                           points_only : bool = False, 
-                           polys : int = 3, 
-                           regressor = LASSO_CV_REGRESSOR) -> None:
-    print()
-    reg_df = base_df.loc[base_df['FantPos'] == position].copy()
-    base_vars = ['A_pred_points',
-                'A_std_error',
-                'num_others',
-                'avg_pred_points_other',
-                'avg_std_error_other'
-                ]
+def _getConfigRegXVars(position: str) -> List[str]:
+    x_vars = ['A_pred_points',
+            'A_std_error',
+            'num_others',
+            'avg_pred_points_other',
+            'avg_std_error_other'
+            ]
     if position in['RB','WR']:
-        base_vars = base_vars + ['B_pred_points', 'B_std_error']
-    if points_only:
-        base_vars = [i for i in base_vars if 'error' not in i]
+        x_vars = x_vars + ['B_pred_points', 'B_std_error']
+    return x_vars
+
+def _splitDatasetByIdentifier(df: pd.DataFrame,
+                              identifiers: List[str]
+                              ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df['unique_id'] = ''
+    for col in identifiers:
+        df['unique_id'] += '_' + df[col].astype(str)
     
-    st = time.time()
-    adp_pts_model_0 = split_and_try_model(reg_df, scoringType.points_name(), base_vars, polys = polys, regressor = regressor)
-    logger.info(f"Time to fit model: {time.time() - st}")
+    unique_ids = df['unique_id'].unique()
+    train_ids, test_ids = train_test_split(unique_ids, test_size=0.2, random_state=42)
+    train_df = df[df['unique_id'].isin(train_ids)].copy()
+    test_df = df[df['unique_id'].isin(test_ids)].copy()
+    return train_df, test_df
+
+def evaluateOverallR2(results_path: str = '../../data/regression/rosterConfig/allResults.csv',
+                      model: sklearn.pipeline.Pipeline = LASSO_CV_REGRESSOR,
+                      poly: int = 1) -> None:
+    logger.info(f"Getting overall aggregated R2 for ByPositionLinear results at poly {poly}")
+    name = 'ByPositionLinear'
+    x_description = """Aggregated result adding regressions on each position. Position-level regression is y ~ roster Config variables"""
+    df = _prepSimple()
+
+    _, test_df = _splitDatasetByIdentifier(df, ['year', 'league', 'team'])
+    test_df['pred'] = 0.0
+    clean_model_name = getModelName(model, poly)
     
-    if save:
-        print('dumping results...')
-        path_name = f'../../data/regression/rosterConfig/results/{position}_rosterconfigreg_params.joblib'
-        joblib.dump(adp_pts_model_0, path_name)
+    for position in ['QB','RB','WR','TE','FLEX']:
+        x_vars = _getConfigRegXVars(position)
+        results_file_name = name.lower() + '_' + clean_model_name + "_" + position
+        model = joblib.load(f'../../data/regression/rosterConfig/parameters/byPosition/{results_file_name}.joblib')
+        test_df.loc[test_df['FantPos'] == position, 'pred'] = model.predict(test_df.loc[test_df['FantPos'] == position, x_vars]).astype('float64')
     
-    reg_df['pred'] = adp_pts_model_0.predict(reg_df[base_vars])
-    base_df.loc[reg_df['index'], 'pred'] = reg_df['pred']
-    return base_df
+    team_df = test_df[['unique_id', scoringType.points_name(), 'pred']].groupby(['unique_id']).sum().reset_index(drop = True)
+    test_r2 = r2_score(team_df[scoringType.points_name()], team_df['pred'])
+    print(test_r2)
+    
+    model_name = getModelName(model, poly, shortVersion=False)
+    df_row = createDfRow(name, x_description, model_name, None, None, test_r2, 'Aggregated')
+    writeDfRowToFile(df_row, results_path)
+
+def doRosterConfigByPosition(save_coefs: bool = True,
+                                   save_coefs_path: str = '../../data/regression/rosterConfig/parameters/byPosition/{}.joblib',
+                                   results_path: str = '../../data/regression/rosterConfig/allResults.csv',
+                                   model: sklearn.pipeline.Pipeline = LASSO_CV_REGRESSOR) -> None:
+    name = "ByPositionLinear"
+    x_description = """Estimated by doing regressions on each position. Position-level regression is y ~ roster Config variables"""
+
+    logger.info("Starting ByPositionLinear")
+    train_df, test_df = _splitDatasetByIdentifier(_prepSimple(), ['year', 'league', 'team'])
+
+    for poly in [1, 2, 3]:
+        print("Poly: ", poly)
+        for position in ['QB','RB','WR','TE','FLEX']:
+            print(f'====================\n{position}\n====================')            
+            # Define variables
+            x_vars = _getConfigRegXVars(position)
+            y_var = scoringType.points_name()
+            
+            # Split Data
+            train = train_df[train_df['FantPos'] == position]
+            test = test_df[test_df['FantPos'] == position]
+            base_x : Tuple[pd.DataFrame, pd.DataFrame] = (train[x_vars].copy(), test[x_vars].copy())
+            y_pts : Tuple[pd.DataFrame, pd.DataFrame] = (train[y_var].copy(), test[y_var].copy())
+
+            model_res, train_r2, test_r2, time_to_finish = try_model(base_x, y_pts, polys = poly, regressor = model)
+            model_name = getModelName(model, poly, shortVersion=False)
+            df_row = createDfRow(name, x_description, model_name, time_to_finish, train_r2, test_r2, position)
+            writeDfRowToFile(df_row, results_path)
+        
+            if save_coefs:
+                clean_model_name = getModelName(model, poly)
+                results_file_name = name.lower() + '_' + clean_model_name + "_" + position
+                joblib.dump(model_res, save_coefs_path.format(results_file_name))             
+    
+        evaluateOverallR2(results_path, model, poly)
+        
 
 def initializeModels(path = f'../../data/regression/rosterConfig/results') -> Dict[str, sklearn.pipeline.Pipeline]:
     positions = ['QB','RB','TE','WR','FLEX']
@@ -316,20 +318,27 @@ if __name__ == '__main__':
     path = pathlib.Path(__file__).parent.resolve()
     os.chdir(path)
     
+    # standard_nn_model = MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.005, learning_rate_init= 0.0005)
+    # calls = [doRosterConfigStarter, doRosterConfigOneLine, doRosterConfigSimple]
+    # for call in calls:
+    #     try:
+    #         logger.info(f"Running {call}")
+    #         call()
+    #     except Exception as e:
+    #         logger.error(f"Error in {call}: {e}")
+    #     try:
+    #         logger.info(f"Running {call} with Neural Network")
+    #         call(model = standard_nn_model)
+    #     except Exception as e:
+    #         logger.error(f"Error in {call}: {e}")
 
-    standard_nn_model = MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.005, learning_rate_init= 0.0005)
-    calls = [doRosterConfigStarter, doRosterConfigOneLine, doRosterConfigSimple]
-    for call in calls:
-        try:
-            logger.info(f"Running {call}")
-            call()
-        except Exception as e:
-            logger.error(f"Error in {call}: {e}")
-        try:
-            logger.info(f"Running {call} with Neural Network")
-            call(model = standard_nn_model)
-        except Exception as e:
-            logger.error(f"Error in {call}: {e}")
+    # doRosterConfigByPosition()
+    # MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.001, learning_rate_init= 0.005)
+    standard_nn_model = MLPRegressor(max_iter=300, hidden_layer_sizes=(100, 100), activation='tanh', solver='adam', alpha=0.005, learning_rate_init= 0.005)
+    # doRosterConfigByPosition(standard_nn_model)
+
+    doRosterConfigOneLine(standard_nn_model)
+    # evaluateOverallR2()
 
     # doRosterConfigOneLine(model = standard_nn_model)
     # 2024-07-21 14:17:29,094 - INFO - regression - try_model - Finished fitting model in 731.7506449222565 seconds
